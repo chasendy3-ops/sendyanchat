655581b8a6a3bd9c8b11c434b76738198d5bb47d
chasendy3-ops <chasendy3@gmail.com>
2026-06-12 22:26:08 +0700

    Add src/config/constants.js

---
+++ src/config/constants.js
@@ -0,0 +1,25 @@
+export const colors = {
+  primary: '#2196f3',
+  border: '#565656',
+  red: '#EF5350',
+  pink: '#EC407A',
+  teal: '#26A69A',
+  grey: '#BDBDBD',
+};
+
+export const spacing = {
+  xxs: 4,
+  xs: 8,
+  sm: 12,
+  md: 16,
+  lg: 20,
+  xl: 24,
+  xxl: 32,
+};
+
+export const layout = {
+  cardRadius: 16,
+  fabOffset: spacing.md,
+  pageInset: spacing.md,
+  pageTopInset: 16,
+};
